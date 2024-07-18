0f95ead0923bceb2f94563076d44c408c0a4da20
gerimad <greg.madarasz@gmail.com>
2024-07-18 16:41:18 +0200

    js conquered works well

--- pontosje_backend/api/process_grammar.py
+++ pontosje_backend/api/process_grammar.py
@@ -4,5 +4,4 @@ from celery import shared_task
 
 @shared_task
 def correct_grammar(text):
-    time.sleep(1)
     return text
--- pontosje_backend/api/urls.py
+++ pontosje_backend/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TextContentViewSet
+from .views import TextContentViewSet, task_status
 
 router = DefaultRouter()
 router.register(r"text-content", TextContentViewSet, basename="text-content")
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("task_status/<str:task_id>/", task_status, name="task-status"),
 ]
